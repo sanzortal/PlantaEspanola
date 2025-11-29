741455ba15f746a88dd1a37dda244953cdd387a8
sanzortal <sanaazorrouktalidi@gmail.com>
2025-11-29 04:28:02 +0100

    Funcionamiento del menu
    Funcionamiento del menu principal, con el boton de opciones y logros añadidos.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,11 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    
+    public int numberScene;
+    public GameObject panelOptions;
+    public GameObject panelAchievements;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -16,13 +19,30 @@ public class Menu : MonoBehaviour
         
     }
 
-    public void Play()
+    public void ChangeScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(numberScene);
+    }
+
+    public void Options()
+    {
+        panelOptions.SetActive(true);
+    }
+
+    public void ReturnBack()
+    {
+        panelOptions.SetActive(false);
+        panelAchievements.SetActive(false);
+    }
+
+    public void Achievements()
+    {
+        panelAchievements.SetActive(true);
     }
 
     public void Exit()
     {
         Application.Quit();
+        Debug.Log("Hasta nunca...");
     }
 }
